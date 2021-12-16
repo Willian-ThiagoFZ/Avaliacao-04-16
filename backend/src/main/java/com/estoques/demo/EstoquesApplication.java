220298f2e64332ac0d890500bc161266593f4b07
Willian <willianthiagofozz@hotmail.com>
2021-12-16 15:07:11 -0300

    init back-end project

---
+++ backend/src/main/java/com/estoques/demo/EstoquesApplication.java
@@ -0,0 +1,13 @@
+package com.estoques.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoquesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/estoques/demo/EstoquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoques.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
